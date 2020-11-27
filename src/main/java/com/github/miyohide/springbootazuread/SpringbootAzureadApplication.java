d5e8a5b0670d0b8562f9f074595341fe5999a3ad
Hidenori Miyoshi <miyohide@gmail.com>
2020-11-27 21:18:11 +0900

    create initial file using Spring Initializr

---
+++ src/main/java/com/github/miyohide/springbootazuread/SpringbootAzureadApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springbootazuread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzureadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAzureadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/miyohide/springbootazuread/SpringbootAzureadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springbootazuread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
